b15fb571daaa13c020e91ea70943275007a34ecf
db1600800 <db1600800@163.com>
2015-06-17 23:11:58 +0800

    firstdfdsfdff

---
+++ Philately/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Philately
+//
+//  Created by gdpost on 15/5/13.
+//  Copyright (c) 2015年 gdpost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
